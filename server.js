eb475c77fa19c1209e562730634b9d0ca09301b1
Amala Hema <amalahema@gmail.com>
2023-05-08 16:44:56 -0400

    prompt user fuction added with switch case

---
+++ server.js
@@ -0,0 +1,81 @@
+//add all the dependecies
+
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+require("console.table")
+
+
+//establish the mysql connection
+
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    port: 3306,//special port number for mysql
+    username:'root',
+    password:'',
+    database:''
+});
+
+// if error in connection
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connection ");
+    //function name for prompt
+});
+
+//create the function for user prompt
+
+function promptUser()
+{
+    inquirer.prompt({
+        //set the list of questions
+        //name given as selectedChoice 
+        //message  is as usaual what u like to show
+        type: "list",
+        name: selectedChoice,
+        message: "What would you like to do?",
+        //list of questios set as an array
+        Choices: [
+            "View Employees",
+            "View Employees by Department",
+            "Add Employee",
+            "Remove Employees",
+            "Update Employee Role",
+            "Add Role",
+            "End"
+
+       ]
+    })
+    .then (function({selectedChoice}){
+       switch(selectedChoice)
+       {
+        case "View Employees":
+          viewEmployee();
+          break;
+
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+      
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employees":
+          removeEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+       }
+    });
+}
